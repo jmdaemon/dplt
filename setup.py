fed452d6c28d60d89693b2768f7bd087091243b5
Joseph Diza <josephm.diza@gmail.com>
2021-11-02 12:19:11 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pylab-jmd",
+    version="0.1.0",
+    author="Joseph Diza",
+    author_email="josephm.diza@gmail.com",
+    description="Python library and executable for conducting lab reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmdaemon/pylab",
+    project_urls={
+        "Bug Tracker": "https://github.com/jmdaemon/sap/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "pylab"},
+    packages=find_packages(where="pylab"),
+    python_requires=">=3.6",
+    py_modules=['pylab', 'util', 'calc'],
+    install_requires=[
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pylab = pylab_cmd:main',
+        ],
+    },
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
