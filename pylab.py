c8fb469c0d6cc24ce0cc412b5b4ce91e7c549bdd
Joseph Diza <josephm.diza@gmail.com>
2021-11-02 01:20:14 -0700

    Initial commit

---
+++ pylab.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import argparse
+import pylab.util
+
+parser = argparse.ArgumentParser(description='Executable file for Labs')
+parser.add_argument('input'     , type=str, help='File path to some input.csv')
+parser.add_argument('--csv'     , type=str, help='Change output filename of lab data [Default: Data.csv]')
+parser.add_argument('--graph'   , type=str, help='Change output filename of graph data [Default: Graph.csv]')
+
+args = parser.parse_args()
+
+input_csv   = args.input
+output_csv  = args.csv
+graph_csv   = args.graph
+
+input_csv = input_csv if input_csv else 'Data.csv'
+graph_csv = graph_csv if graph_csv else 'Graph.csv'
+pylab.util.output(input_csv, output_csv, graph_csv)
