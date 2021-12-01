97dc99f9b4a24020f513c9889345d83e838e8266
Joseph Diza <josephm.diza@gmail.com>
2021-12-01 01:08:18 -0800

    Remove dedup between export_graph(), export_data() with export()

--- src/dxp/util.py
+++ src/dxp/util.py
@@ -13,15 +13,15 @@ def head(data: pd.DataFrame, inputs: pd.DataFrame):
     print(f'==== Output DataFrame ====\n\n{data.head()}\n')
     print(f'==== Input DataFrame ====\n\n{inputs.head()}\n')
 
-def shape(df: pd.DataFrame):
-    shape = df.shape[0]
-    return shape
-
 def display(df: pd.DataFrame, title='Output DataFrame', *args, **kwargs):
     print(f'==== {title} ====\n')
     print(f'{df}\n')
 
 # Populate
+def shape(df: pd.DataFrame):
+    shape = df.shape[0]
+    return shape
+
 def copy(data: pd.DataFrame, inputs: pd.DataFrame, cols: list[str]):
     for col in cols:
         data[col] = inputs[col].copy()
@@ -54,13 +54,11 @@ def err(data: pd.DataFrame, inputs: pd.DataFrame, sigmas: np.ndarray, length: in
     data[col] = pd.DataFrame(deltas)
     return data, deltas
 
-# Export
-def export_data(df: pd.DataFrame, dest):
-    output_data = df
-    display(output_data)
-    output_data.to_csv(dest, index=False)
-
-def export_graph(df, cols: list[str], dest):
-    output_graph = df[cols]
-    display(output_graph)
-    output_graph.to_csv(dest, index=False)
+def export(df, dest, cols):
+    output: pd.DataFrame
+    if len(cols) == 1:
+        output = df
+    else:
+        output = df[cols]
+    display(output)
+    output.to_csv(dest, index=False)
