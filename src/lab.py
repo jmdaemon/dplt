51a4a3dbf867ee081a42ec0fc30654c906172e60
Joseph Diza <josephm.diza@gmail.com>
2021-11-30 19:14:10 -0800

    Move lab specific populate code to runtime loaded pop.py file

--- src/lab.py
+++ src/lab.py
@@ -1,45 +1,30 @@
 from util import *
+import importlib.util
 
-def populate(data: pd.DataFrame, inputs: pd.DataFrame,
-             output_csv: str, graph_csv: str):
-    # ==== Copying ====
-    cols = ['L', 'dL', 't1', 't2', 't3', 't4', 't5']
-    data = copy(data, inputs, cols)
-
-    # ==== Calculations ====
-
-    display(data)
-    ids = ['t1', 't2', 't3', 't4', 't5']
-    shape = inputs['t1'].shape
-    length = 5
-
-    # Populate
-    data, avgs, fvals = avg(data, inputs, shape, ids)
-    display(data, title='Average Acceleration')
-    data, sigmas = sd(data, inputs, shape, avgs, fvals)
-    display(data, title='Standard Deviation')
-    data, sigmas = err(data, inputs, sigmas, length, 'dabar')
-    display(data, title='Error in Acceleration')
-
-    # Significant Figures
-    df = data.apply(signif)
-    display(df)
-
-    # Export
-    print(output_csv, graph_csv)
-    export_data(df, output_csv)
-
-def output(csv_input: str,
-           output_csv: str,
-           graph_csv: str,
+def output(fp: str, csv_input: str, output_csv: str, graph_csv: str,
            *args, **kwargs):
     '''
-    output takes in some input.csv filename, reads the file
+    Reads values from a csv file and outputs a
+    csv file with additional calculated data and columns.
+
+    Parameters
+        fp          - path to current lab directory
+        csv_input   - path to lab data in csv format
+        output_csv  - Name/Destination of populated csv data
+        graph_csv   - Name/Destination of the output csv graph
     '''
 
-    columns = ['L', 'dL', 't1', 't2', 't3', 't4', 't5']
     inputs  = pd.read_csv(csv_input)
+    columns = inputs.columns.tolist()
     data    = pd.DataFrame(columns=columns, index=inputs.index)
     display(data    , title='Output DataFrame')
     display(inputs  , title='Input DataFrame')
-    populate(data, inputs, output_csv, graph_csv)
+
+    def module_from_file(module_name, file_path):
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+
+    pop = module_from_file("pop", f'{fp}/pop.py')
+    pop.populate(data, inputs, output_csv, graph_csv)
