67990c3d777b2e92e9ae8eedd945f640724718b2
Joseph Diza <josephm.diza@gmail.com>
2022-02-15 20:12:48 -0800

    Prune dead code & fix testname typo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,27 +18,7 @@ def test_copy():
     sliceidf = pd.DataFrame(idf['col1'])
     assert (du.copy(idf, odf, ['col1']).equals(sliceidf)), 'Should copy slices of DataFrame'
 
-
-# def merge(df1, df2, cols):
-    # # arrdf = pd.DataFrame(arr, columns=[col])
-    # # arrdf = pd.DataFrame(arr)
-    # # df[col] = pd.DataFrame(arr)
-    # # return df.merge(arrdf, on=col)
-    # df1 = df2[cols].copy()
-    # return df1
-# def test_merge():
-    # idf = setupdf()
-    # # data = np.array([3, 4])
-    # data = pd.DataFrame({'col1': np.array([3, 4])})
-    # # odf = pd.DataFrame(data={'col1': [3, 4]})
-    # # outputdf = du.merge(idf, data, 'col1')
-    # outputdf = merge(idf, data, ['col1'])
-    # print(outputdf)
-    # expect = pd.DataFrame(data={'col1': [1,2,3,4]})
-
-    # assert(outputdf.equals(expect)), 'Should merge values from numpy array'
-
-def test_expand():
+def test_colavg():
     df = setupdf()
     assert(du.colavg(df, 'col1') == 1.5), 'Should calculate the average of a DataFrame column'
 
