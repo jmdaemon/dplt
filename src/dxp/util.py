975489275ebf305c18457454c694fb4a3945e01b
Joseph Diza <josephm.diza@gmail.com>
2022-02-15 20:45:09 -0800

    Prune obsolete Data class

--- src/dxp/util.py
+++ src/dxp/util.py
@@ -1,28 +1,11 @@
 import numpy as np
 import pandas as pd
 
-from dataclasses import dataclass
-
 class Sample:
     def __init__(self, fp: str):
         self.fp = fp
         self.csv = pd.read_csv(fp)
         self.df = pd.DataFrame()
-
-@dataclass
-class Data:
-    ''' Stores the Input and Output Data Frames '''
-    odf: None # Output DataFrame
-    idf: None # Input DataFrame
-
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def display(self, title='Output DataFrame', df='odf'):
-        print(f'==== {title} ====\n\n{self[df]}\n')
 
 def show(df: pd.DataFrame, msg='DataFrame'):
     ''' Displays the contents of a DataFrame in a block format '''
@@ -57,7 +40,6 @@ def slice(df: pd.DataFrame, cols: list[str]):
     ''' Return a slice of the DataFrame's columns '''
     return df[cols]
 
-# Numpy Arrays
 def colavg(df: pd.DataFrame, col='avg') -> np.ndarray:
     ''' Return the average of an entire column '''
     values = df[col].to_numpy()
