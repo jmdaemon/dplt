8a7619d1fece23142858a2f083d21016831da1df
Joseph Diza <josephm.diza@gmail.com>
2022-02-15 21:18:36 -0800

    Use pytest fixture in tests

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name="dxp",
+    name='dxp',
     license='MIT',
-    author="Joseph Diza",
-    author_email="josephm.diza@gmail.com",
-    description="Library for analyzing CSV data",
+    author='Joseph Diza',
+    author_email='josephm.diza@gmail.com',
+    description='Library for analyzing CSV data',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmdaemon/dxp",
-    project_urls={ "Bug Tracker": "https://github.com/jmdaemon/dxp/issues", },
+    long_description_content_type='text/markdown',
+    url='https://github.com/jmdaemon/dxp',
+    project_urls={ 'Bug Tracker': 'https://github.com/jmdaemon/dxp/issues', },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
     ],
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     py_modules=['dxp', 'dxp.util', 'dxp.cli'],
     install_requires=['argparse', 'pandas', 'numpy', 'wora'],
     scripts=['scripts/dplt', 'scripts/tpose'],
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,24 @@
 import dxp.util as du
 import pandas as pd
 import numpy as np
+import pytest
 
-def setupdf():
+@pytest.fixture
+def df():
     d = {'col1': [1, 2], 'col2': [3, 4]}
     return pd.DataFrame(data=d)
 
-def test_show(capsys):
-    df = setupdf()
+def test_show(capsys, df):
     du.show(df)
     captured = capsys.readouterr()
     assert(captured is not None), 'Should output contents of DataFrame'
 
-def test_catslice():
-    idf = setupdf()
+def test_catslice(df):
     odf = pd.DataFrame()
-    sliceidf = pd.DataFrame(idf['col1'])
-    assert (du.catslice(idf, odf, ['col1']).equals(sliceidf)), 'Should concat slices of DataFrame'
+    sliceidf = pd.DataFrame(df['col1'])
+    assert (du.catslice(df, odf, ['col1']).equals(sliceidf)), 'Should concat slices of DataFrame'
 
-def test_colavg():
-    df = setupdf()
+def test_colavg(df):
     assert(du.colavg(df, 'col1') == 1.5), 'Should calculate the average of a DataFrame column'
 
 def test_expand():
