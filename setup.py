37398912be4e60ced8d5e8798cd8fa6f1a94a786
Joseph Diza <josephm.diza@gmail.com>
2022-04-01 16:07:25 -0700

    Fix typo in licenses field

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='dxp',
-    license='MIT',
+    license='GPL-3.0',
     author='Joseph Diza',
     author_email='josephm.diza@gmail.com',
     description='Library for analyzing CSV data',
